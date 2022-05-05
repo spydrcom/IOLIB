a08335088b90291eb8da1dd639387e36e94bfa61
Michael Druckman <spydrcom@gmail.com>
2022-05-05 12:50:55 -0700

    JXR menu code
    old MDI test changed to use JXR for menu

---
+++ testing/net/myorb/gui/tests/MenuBarTest.java
@@ -0,0 +1,43 @@
+
+package net.myorb.gui.tests;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import net.myorb.jxr.JxrParser;
+import net.myorb.jxr.JxrPrimitives;
+import net.myorb.jxr.JxrReport;
+
+public class MenuBarTest
+{
+
+	public static void main (String... args) throws Exception
+	{
+
+//		JxrPrimitives.SymbolTable symbols = JxrParser.read ("data/JxrTest.xml");
+
+//		JxrPrimitives.SymbolTable symbols = JxrParser.read ("data/BeanTest.xml");
+
+//		JxrPrimitives.SymbolTable symbols = JxrParser.read ("data/NestedTest.xml");
+
+//		System.out.println ("SYMBOLS: "+symbols);
+
+		JxrPrimitives.SymbolTable symbols =
+				JxrParser.read ("JxrTests/MenuBarTest.xml", null);
+		JxrReport.dump (symbols);
+
+	}
+
+	public ActionListener getAction ()
+	{
+		return new ActionListener ()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				System.out.println ("OK NOW");
+			}
+		};
+	}
+
+}
