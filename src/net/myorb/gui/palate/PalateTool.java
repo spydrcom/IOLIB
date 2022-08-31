554b66d60f7b9a87685d6f46ca1bfd014cc3a35b
Michael Druckman <spydrcom@gmail.com>
2022-08-31 13:46:38 -0700

    palate tool infrastructure
    new package added for palate tool support
    empty class added as holding place for tool

---
+++ src/net/myorb/gui/palate/PalateTool.java
@@ -0,0 +1,15 @@
+
+package net.myorb.gui.palate;
+
+import net.myorb.gui.components.SimpleScreenIO;
+
+/**
+ * the palate tool component for application GUI management
+ * @author Michael Druckman
+ */
+public class PalateTool extends SimpleScreenIO.Panel
+{
+
+	private static final long serialVersionUID = -5619629168067179759L;
+
+}
---
+++ src/net/myorb/gui/palate/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Tool for color palate maintenance
+ * @author Michael Druckman
+ */
+package net.myorb.gui.palate;
