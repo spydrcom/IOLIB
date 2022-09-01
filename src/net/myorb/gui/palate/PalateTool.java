8afdb7c7bcb56e6059d718a89825518280f0ab70
Michael Druckman <spydrcom@gmail.com>
2022-08-31 17:38:51 -0700

    Update PalateTool.java
    first changes for dynamic palate

--- src/net/myorb/gui/palate/PalateTool.java
+++ src/net/myorb/gui/palate/PalateTool.java
@@ -1,6 +1,8 @@
 
 package net.myorb.gui.palate;
 
+import net.myorb.gui.graphics.ColorDisplays;
+import net.myorb.gui.graphics.ColorPropertiesProcessor;
 import net.myorb.gui.components.SimpleScreenIO;
 
 /**
@@ -10,6 +12,11 @@ import net.myorb.gui.components.SimpleScreenIO;
 public class PalateTool extends SimpleScreenIO.Panel
 {
 
+	public PalateTool ()
+	{
+		ColorPropertiesProcessor.processColorList ();
+		this.add (ColorDisplays.getGBmatrixPanel ());
+	}
 	private static final long serialVersionUID = -5619629168067179759L;
 
 }
