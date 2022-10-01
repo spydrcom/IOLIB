986a71acccd616ecd406bc0fd70dbd3aa3aea917
Michael Druckman <spydrcom@gmail.com>
2022-10-01 14:27:26 -0700

    Update ConfigurationParser.java
    Interpreter interface added allowing non-standard processing
    QOT token type now process as special Interpreter
    CalcLib now using non-standard Interpreter

--- src/net/myorb/data/abstractions/ConfigurationParser.java
+++ src/net/myorb/data/abstractions/ConfigurationParser.java
@@ -13,6 +13,19 @@ public class ConfigurationParser
 {
 
 	/**
+	 * mechanism for selection of token interpretation
+	 */
+	public interface Interpreter
+	{
+		/**
+		 * determine use for a token of a command stream
+		 * @param symbol the name assigned to the token value
+		 * @param token the token to process
+		 */
+		void process (String symbol, CommonCommandParser.TokenDescriptor token);
+	}
+
+	/**
 	 * parse configuration parameters from a token list
 	 * @param tokens a token list from the command line
 	 * @param parameters the configuration map to build
@@ -27,6 +40,25 @@ public class ConfigurationParser
 	}
 
 	/**
+	 * pass token pairs to interpreter
+	 * @param tokens the list of tokens to process
+	 * @param interpreter the interpreter object to use
+	 */
+	public static void process
+		(
+			List <CommonCommandParser.TokenDescriptor> tokens,
+			Interpreter interpreter
+		)
+	{
+		int n = 0, count = tokens.size () - 1; String sym;
+		while (n < count)
+		{
+			sym = tokens.get (n++).getTokenImage ();
+			interpreter.process (sym, tokens.get (n++));
+		}
+	}
+
+	/**
 	 * map configuration parameters from the parser
 	 * @param tokens a token list from the command line
 	 * @param parameters the configuration map to build
@@ -37,15 +69,34 @@ public class ConfigurationParser
 			Map<String, Object> parameters
 		)
 	{
-		int n = 0;
-		String sym, val;
-		while (n+1 < tokens.size())
-		{
-			sym = tokens.get (n++).getTokenImage ();
-			val = strip (tokens.get (n++).getTokenImage ());
-			parameters.put (sym, val);
-		}
+		process
+		(
+			tokens,
+			(s, t) ->
+				{
+					if (t.getTokenType () != CommonCommandParser.TokenType.QOT)
+					{ throw new RuntimeException ("Expected string: " + t.getTokenImage ()); }
+					addParameterValue (parameters, t, s);
+				}
+		);
+	}
+
+	/**
+	 * quoted text to be added to configuration
+	 * @param toConfiguration configuration map to add to
+	 * @param fromToken the token holding the text value
+	 * @param toBeCalled the name of the symbol
+	 */
+	public static void addParameterValue
+		(
+			Map<String, Object> toConfiguration,
+			CommonCommandParser.TokenDescriptor fromToken,
+			String toBeCalled
+		)
+	{
+		toConfiguration.put (toBeCalled, strip (fromToken.getTokenImage ()));
 	}
 	public static String strip (String text) { return CommonCommandParser.stripQuotes (text); }
 
 }
+
