1ab28afd61bbf3f87a79bba786e0fa9c9f55bf7a
Michael Druckman <spydrcom@gmail.com>
2023-05-10 13:28:21 -0700

    Update BackgroundTask.java
    added support for task completion event notification
    override for done method triggers notifier

--- src/net/myorb/gui/BackgroundTask.java
+++ src/net/myorb/gui/BackgroundTask.java
@@ -11,6 +11,24 @@ public class BackgroundTask extends SwingWorker<Object, Object>
 {
 
 	/**
+	 * task completion event notification mechanism
+	 */
+	public interface Completed
+	{
+		/**
+		 * method called indicating task has completed
+		 */
+		void processCompletion ();
+	}
+
+	public BackgroundTask (Runnable task, Completed completionProcessor)
+	{ this (task); this.setCompletionProcessor (completionProcessor); }
+
+	protected Completed completionProcessor = null;
+	public void setCompletionProcessor (Completed C)
+	{ this.completionProcessor = C; }
+
+	/**
 	 * @param task the implementation of the task
 	 */
 	public BackgroundTask (Runnable task)
@@ -36,12 +54,18 @@ public class BackgroundTask extends SwingWorker<Object, Object>
 	}
 
 	/* (non-Javadoc)
+	 * @see javax.swing.SwingWorker#done()
+	 */
+	public void done ()
+	{
+		if (completionProcessor == null) return;
+		completionProcessor.processCompletion ();
+	}
+
+	/* (non-Javadoc)
 	 * @see javax.swing.SwingWorker#doInBackground()
 	 */
 	protected Object doInBackground () throws Exception
-	{
-		task.run ();
-		return null;
-	}
+	{ task.run (); return null; }
 
 }
