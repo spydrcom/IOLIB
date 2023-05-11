c06b9d38fb1b377426ef092f76cb4a7877836182
Michael Druckman <spydrcom@gmail.com>
2023-05-11 16:28:58 -0700

    Update ColorNames.java
    refactor implementation of temperature color model
    value and range treatments moved to ColorAssignment class

--- src/net/myorb/gui/graphics/ColorNames.java
+++ src/net/myorb/gui/graphics/ColorNames.java
@@ -241,35 +241,6 @@ public class ColorNames extends SimpleScreenIO
 	public static ColorMap hsb100 () { return hsbAlgorithmMap; }
 
 
-	/**
-	 * get color based on value range
-	 * @param value a value in the range lo..hi
-	 * @param lo the low end of the value domain
-	 * @param hi the high end of the value domain
-	 * @return the Color object for given value
-	 */
-	public static Color getTemperatureColorFrom (double value, double lo, double hi)
-	{
-		double range = hi - lo;
-		double adjustedValue = value - lo;
-		return getTemperatureColorFor (adjustedValue / range);
-	}
-
-
-	/**
-	 * get color for standard 0..1 range
-	 * @param value the requested entry specified as float range 0..1
-	 * @return the Color object for given value
-	 */
-	public static Color getTemperatureColorFor (double value)
-	{
-		if ( value < 0.0 || value > 1.0 )
-		{ throw new RuntimeException ("Temperature Color request out of range 0..1"); }
-		Number index = 100.0 * value; int entryNumber = index.intValue ();
-		return TemperatureColorIndex.get (entryNumber);
-	}
-
-
 	// colors from Java system database
 
 
