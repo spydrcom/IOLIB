d77eb05826c150920deb025d7043ef9ae90348d6
Michael Druckman <spydrcom@gmail.com>
2023-06-06 15:51:36 -0700

    Update MarketDataFileParser.java
    refactor adding layers to clarify code
    comments added and clarified

--- src/net/myorb/gui/graphics/markets/data/MarketDataFileParser.java
+++ src/net/myorb/gui/graphics/markets/data/MarketDataFileParser.java
@@ -2,6 +2,7 @@
 package net.myorb.gui.graphics.markets.data;
 
 import net.myorb.gui.graphics.markets.data.MarketQuoteParameters.*;
+import net.myorb.gui.graphics.markets.data.MarketQuoteParameters;
 import net.myorb.data.conventional.*;
 
 import java.util.List;
@@ -22,21 +23,51 @@ public class MarketDataFileParser
 
 
 	/**
-	 * parse a historical market source
-	 * @param source path to source file
-	 * @param market name of the market quoted
-	 * @return the list of bars parsed from file
-	 * @throws Exception for IO errors
+	 * lookup name from allowed choices
+	 * @param name the name of the file extension
+	 * @return item of the Type list
 	 */
-	public static List<OHLCV.Bar> read (String source, String market) throws Exception
+	public static Type identifyType (String name)
 	{
-		File f;
-		int dot = source.lastIndexOf ('.');
-		Type type = Type.valueOf (source.substring (dot+1).toUpperCase ());
-		if (!(f = new File (source)).exists ()) return null;
+		return Type.valueOf (name);
+	}
+
+
+	/**
+	 * identify file type from path
+	 * @param path the full path to the file
+	 * @return the identified type
+	 */
+	public static Type getTypeFrom (String path)
+	{
+		try
+		{
+			int dot = path.lastIndexOf ('.');
+			String fileType = path.substring (dot + 1);
+			return identifyType ( MarketQuoteParameters.stripUC (fileType) );
+		}
+		catch (Exception e)
+		{
+			throw new RuntimeException ("Invalid file type for Historical archive");
+		}
+	}
+
+
+	/**
+	 * parse a file of quote histories
+	 * @param market name of the market quoted
+	 * @param f the file identified as market history for period
+	 * @param t the file type of the historical source
+	 * @return the parsed series of quote bars
+	 * @throws Exception for any error
+	 */
+	public static OHLCV.Series parse (String market, File f, Type t) throws Exception
+	{
+		if ( ! f.exists () )
+		{ throw new RuntimeException ("Historical archive not found"); }
 		OHLCV.Series series = new OHLCV.Series ();
 
-		switch (type)
+		switch (t)
 		{
 			case CSV: case CSVR:	new CSV ().parse (f, series); break;
 			case TDF: case TDFR:	new TDF ().parse (f, series); break;
@@ -44,7 +75,18 @@ public class MarketDataFileParser
 		}
 
 		series.setMarketName (market);
+		return series;
+	}
 
+
+	/**
+	 * order parsed data according to type
+	 * @param series the parsed series of data points
+	 * @param type the type of the source file read
+	 * @return the bars reordered as appropriate
+	 */
+	public static List <OHLCV.Bar> selected (OHLCV.Series series, Type type)
+	{
 		switch (type)
 		{
 			case YFH:
@@ -56,13 +98,29 @@ public class MarketDataFileParser
 
 
 	/**
-	 * identify file type assuming YAHOO historical format
+	 * parse a historical market source
+	 * @param source path to source file
+	 * @param market name of the market quoted
+	 * @return the list of bars parsed from file
+	 * @throws Exception for IO errors
+	 */
+	public static List <OHLCV.Bar> read (String source, String market) throws Exception
+	{
+		Type type = getTypeFrom (source);
+		OHLCV.Series series = parse (market, new File (source), type);
+		return selected (series, type);
+	}
+
+
+	/**
+	 * identify file type from configuration
 	 * @param timeFrame the time frame for the data of interest
+	 * @param fileType the file extension identifier
 	 * @return the extension for the time frame
 	 */
-	public static String typeFor (Periods timeFrame)
+	public static String typeFor (Periods timeFrame, String fileType)
 	{
-		return MarketQuoteParameters.frequencyFor (timeFrame) + ".yfh";
+		return MarketQuoteParameters.frequencyFor (timeFrame) + "." + MarketQuoteParameters.strip (fileType);
 	}
 
 
@@ -70,12 +128,15 @@ public class MarketDataFileParser
 	 * construct file path to historical data
 	 * @param market name of the market quoted
 	 * @param timeFrame the time period of interest
+	 * @param fileType the file extension identifier
 	 * @return the path to the data file
 	 * @throws Exception for errors
 	 */
-	public static String pathFor (String market, MarketQuoteParameters.Periods timeFrame) throws Exception
+	public static String pathFor
+	(String market, MarketQuoteParameters.Periods timeFrame, String fileType)
+	throws Exception
 	{
-		return "data/" + market + "-" + typeFor (timeFrame);
+		return "data/" + market + "-" + typeFor (timeFrame, fileType);
 	}
 
 
@@ -83,12 +144,15 @@ public class MarketDataFileParser
 	 * read file of historical data
 	 * @param market name of the market quoted
 	 * @param timeFrame the time period of interest
+	 * @param fileType the file extension identifier
 	 * @return the data parsed into a list of bars
 	 * @throws Exception for any errors
 	 */
-	public static List<OHLCV.Bar> read (String market, MarketQuoteParameters.Periods timeFrame) throws Exception
+	public static List<OHLCV.Bar> read
+	(String market, MarketQuoteParameters.Periods timeFrame, String fileType)
+	throws Exception
 	{
-		return read ( pathFor (market, timeFrame), market );
+		return read ( pathFor (market, timeFrame, fileType), market );
 	}
 
 
