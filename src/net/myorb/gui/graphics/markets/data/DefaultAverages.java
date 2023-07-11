64fbb3ce66e31e95968db0157a8a79b956046a63
Michael Druckman <spydrcom@gmail.com>
2023-07-11 16:11:52 -0700

    Create DefaultAverages.java
    establish default parameters for averages

---
+++ src/net/myorb/gui/graphics/markets/data/DefaultAverages.java
@@ -0,0 +1,56 @@
+
+package net.myorb.gui.graphics.markets.data;
+
+import net.myorb.gui.graphics.markets.data.MarketQuoteParameters.Periods;
+
+/**
+ * averaging periods for time frames chosen
+ * @author Michael Druckman
+ */
+public class DefaultAverages extends MarketQuoteParameters.Averaging
+{
+
+	/* (non-Javadoc)
+	 * @see net.myorb.gui.graphics.markets.data.MarketQuoteParameters.Averaging#getHistoryType()
+	 */
+	public String getHistoryType () { return null; }
+
+	/* (non-Javadoc)
+	 * @see net.myorb.gui.graphics.markets.data.MarketQuoteParameters.Averaging#getBars(net.myorb.gui.graphics.markets.data.MarketQuoteParameters.Periods)
+	 */
+	public int getBars (Periods timeFrame)
+	{
+		switch (timeFrame)
+		{
+			case MONTH: return 12;
+			case HOUR: return 48;
+			default: break;
+		}
+		return 30;
+	}
+
+	/* (non-Javadoc)
+	 * @see net.myorb.gui.graphics.markets.data.MarketQuoteParameters.Averaging#getPeriod(net.myorb.gui.graphics.markets.data.MarketQuoteParameters.Periods)
+	 */
+	public int getPeriod (Periods timeFrame)
+	{
+		switch (timeFrame)
+		{
+			case MONTH: return 12;
+			case HOUR: return 30;
+			default: break;
+		}
+		return 13;
+	}
+
+	/* (non-Javadoc)
+	 * @see net.myorb.gui.graphics.markets.data.MarketQuoteParameters.Averaging#getLiveDate(net.myorb.gui.graphics.markets.data.MarketQuoteParameters.Periods)
+	 */
+	public String getLiveDate (Periods timeFrame) { return null; }
+
+	public DefaultAverages () { super ( new java.util.HashMap <String,String> () ); }
+
+	private static final long serialVersionUID = -4593173041623800375L;
+
+}
+
